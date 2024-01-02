2fa830fca82981a47dc0e27c8e0d1d6f0e71630a
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-02 14:39:52 +0200

    first commit

---
+++ 0x00-ES6_basic/11-main.js
@@ -0,0 +1,4 @@
+import createEmployeesObject from './11-createEmployeesObject.js';
+
+console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
+// { department: 'Software', employees: ['Bob', 'Sylvie'] }
