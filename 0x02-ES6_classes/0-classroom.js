295929fb7d04dd784f8e0e201f83866eef58515b
root <tsamkelo96@gmail.com>
2024-01-04 23:00:05 +0000

    stash

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,5 @@
+export default class ClassRooom {
+	constructor(maxStudentsSize) {
+		this._maxStudentsSize = maxStudentsSize;
+	}
+}
---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,48 @@
+class HolbertonCourse{
+	constructor(name, length, students){
+		if (typeof name !== 'string' || typeeof length !== 'number' || !Array.isArray(students) || !students.every(student => typeof student === 'string')) {
+			throw new TypeError('Invalid attribute inserted please check');
+		}
+		this._name = name;
+		this._length = length;
+		this._students = students;
+	}
+
+	//getter and setter for name
+	get name() {
+		return this._name;
+	}
+
+	set name(newName) {
+		if (typeof newName !== 'string') {
+			throw new TypeError('Name must be a string');
+		}
+		this._name = newName;
+	}
+
+	//getter and setter for length
+	get length() {
+		return this._length;
+	}
+
+	set length(newLength) {
+		if (typeof newLength !== 'number') {
+			throw new TypeError('Length must be a number');
+		}
+		this._length = newLength;
+	}
+	// Getter for students
+  	get students() {
+		return this._students;
+  }
+
+  // Setter for students
+  set students(newStudents) {
+    // Type verification for the array and its elements
+    if (!Array.isArray(newStudents) || !newStudents.every(student => typeof student === 'string')) {
+      throw new TypeError('Students must be an array of strings');
+    }
+    this._students = newStudents;
+  }
+
+}
