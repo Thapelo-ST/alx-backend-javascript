b862257862e650986f44a194501d7fb7270fef12
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-05 03:41:03 +0200

    Fix 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -10,4 +10,20 @@ export default class Building{
     get sqrft(){
         return this._sqrft;
     }
+
+    set sqrft(newSqrft) {
+        if (newSqrft === undefined || newSqrft === null) {
+            throw new Error('Square foot must be defined.');
+        }
+    
+        if (typeof newSqrft !== 'number') {
+            throw new Error('Square foot must be a number.');
+        }
+    
+        if (!isFinite(newSqrft) || newSqrft < 0) {
+            throw new Error('Square foot must be a finite number greater than or equal to 0.');
+        }
+    
+        this._sqrft = newSqrft;
+    }
 }
