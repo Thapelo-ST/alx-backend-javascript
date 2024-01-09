716c61530000a5e08bcb9d6cf51a6dcd7d8ec158
Thapelo-ST <tsamkelo96@gmail.com>
2024-01-09 23:37:50 +0200

    Test 1

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+function getListSudents() {
+    const maping = new Map();
+    const listStudents = [
+        {id: 1, firstName: 'Guillume', location: 'San Francisco'},
+        {id: 2, firstName: 'James', location: 'Columbia'},
+        {id: 5, firstName: 'Serena', location: 'San Francisco'},
+    ];
+    
+    return listStudents;
+}
