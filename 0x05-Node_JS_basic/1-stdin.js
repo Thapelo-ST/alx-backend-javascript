a1c3f819d45c0d22ce92281effde20665e53b362
Thapelo-ST <tsamkelo96@gmail.com>
2024-03-03 00:58:45 +0200

    one more

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,15 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+const readline = require('readline');
 
-if (process.stdin.isTTY) {
-    process.stdin.on('data', (data) => {
-        process.stdout.write(`Your name is: ${data.toString()}`);
-        process.exit();
-    });
-} else {
-    process.stdin.on('data', (data) => {
-        process.stdout.write(`Your name is: ${data.toString()}`);
-        process.exit();
-    });
-    process.on('exit', () => {
-        process.stdout.write('This important software is now closing\n');
-    });
-}
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    process.stdout.write(`Your name is: ${name}\n`);
+    rl.close();
+});
+
+rl.on('close', () => {
+    process.stdout.write('This important software is now closing\n');
+});
