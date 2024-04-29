277e05fd89eb583fa8d0582c1946830c74f6b955
Thapelo-ST <tsamkelo96@gmail.com>
2024-04-29 12:43:48 +0200

    Second push

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+function Greetings() {
+    const line = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    line.question('Welcome to Holberton School, what is your name? \n', (name) => {
+        console.log(`Your name is: ${name}`);
+        line.close();
+    });
+}
+
+if (!process.stdin.isTTY) {
+    console.log("Welcome to Holberton School, what is your name?");
+    let inputName = '';
+    process.stdin.on('data', function (chunk) {
+        inputName += chunk.toString();
+    });
+    process.stdin.on('end', function () {
+        console.log(`Your name is: ${inputName.trim()}`);
+        console.log("The important code is now closing");
+    });
+} else {
+    Greetings();
+}
