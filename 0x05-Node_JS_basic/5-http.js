ed404172ee0722e9a3254748e2ec0c4cc3e5ba53
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-01 12:04:38 +0000

    check

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -17,7 +17,8 @@ const app = http.createServer((req, res) => {
       .catch((error) => {
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/plain');
-        res.end(`Error: ${error.message}`);
+        res.end(`This is the list of our students
+Cannot load the database`);
       });
   } else {
     res.statusCode = 404;
