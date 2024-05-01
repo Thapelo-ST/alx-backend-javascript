7d44f310e6d56884a0eb51896f1080f6b010511c
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-01 11:03:49 +0000

    fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,27 +1,27 @@
 const readline = require('readline');
 
 function Greetings() {
-    const line = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  const line = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-    line.question('Welcome to Holberton School, what is your name? \n', (name) => {
-        console.log(`Your name is: ${name}`);
-        line.close();
-    });
+  line.question('Welcome to Holberton School, what is your name? \n', (name) => {
+    console.log(`Your name is: ${name}`);
+    line.close();
+  });
 }
 
 if (!process.stdin.isTTY) {
-    console.log("Welcome to Holberton School, what is your name?");
-    let inputName = '';
-    process.stdin.on('data', function (chunk) {
-        inputName += chunk.toString();
-    });
-    process.stdin.on('end', function () {
-        console.log(`Your name is: ${inputName.trim()}`);
-        console.log("This important software is now closing");
-    });
+  console.log(`Welcome to Holberton School, what is your name?`);
+  let inputName = '';
+  process.stdin.on('data', function (chunk) {
+    inputName += chunk.toString();
+  });
+  process.stdin.on('end', function () {
+    console.log(`Your name is: ${inputName.trim()}`);
+    console.log(`This important software is now closing`);
+  });
 } else {
-    Greetings();
+  Greetings();
 }
