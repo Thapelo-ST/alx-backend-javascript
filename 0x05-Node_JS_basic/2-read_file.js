8881dd826cd3c85b7cd1810c3fcb57b0d2d6d971
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-01 11:29:26 +0000

    firstName, lastName, age,

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -13,7 +13,7 @@ function countStudents(path) {
     .slice(1);
   console.log(`Number of students: ${numberOfStudents.length}`);
   for (const record of numberOfStudents) {
-    const [firstName, lastName, age, fieldOfStudy] = record.split(",");
+    const [fieldOfStudy] = record.split(",");
     counter[fieldOfStudy] = (counter[fieldOfStudy] || 0) + 1;
   }
 
