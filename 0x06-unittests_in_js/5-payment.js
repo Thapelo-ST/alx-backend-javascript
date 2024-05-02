8196ee3edeb82016a57fcb59c8ec37d6b7083c44
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-02 23:30:16 +0000

    second push

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,7 @@
+const Utils = require("./utils.js");
+function sendPaymentToApi(totalAmount, totalShipping) {
+  const sum = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+  console.log(`The total is: ${sum}`);
+}
+
+module.exports = sendPaymentToApi;
