324be2a544b5e6b82c61922031cfff6022aa0310
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-02 08:44:04 +0000

    fix

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,7 +1,7 @@
 function calculateNumber(a, b) {
   const roundedA = Math.round(a);
   const roundedB = Math.round(b);
-  returnroundedA + roundedB;
+  return roundedA + roundedB;
 }
 
 module.exports = calculateNumber;
