2f0f265f036aed70e448c7b563a3ff8fb5df0026
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-02 10:17:39 +0000

    chai prblems

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,26 @@
+const sinon = require('sinon');
+const expect = require('chai').expect;
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with the correct arguments', () => {
+    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+
+    calculateNumberSpy.restore();
+  });
+
+  it('should log the correct message to the console', () => {
+    const consoleLogSpy = sinon.spy(console, 'log');
+
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledWith('The total is: 120')).to.be.true;
+
+    consoleLogSpy.restore();
+  });
+});
