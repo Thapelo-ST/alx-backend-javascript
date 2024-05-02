6a7fb50152b317f82afeeca0b255baad273c60c6
Taps <105137490+Thapelo-ST@users.noreply.github.com>
2024-05-02 09:44:23 +0000

    final push on 2d

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -3,17 +3,17 @@ function calculateNumber(type, a, b) {
   const roundedB = Math.round(b);
 
   switch (type) {
-    case "SUM":
+    case 'SUM':
       return roundedA + roundedB;
-    case "SUBTRACT":
+    case 'SUBTRACT':
       subtract = roundedA - roundedB;
       return subtract;
-    case "DIVIDE":
-      if (roundedB === 0) return "Error";
+    case 'DIVIDE':
+      if (roundedB === 0) return 'Error';
       divide = roundedA / roundedB;
       return divide;
     default:
-      throw new Error("Invalid type. Type must be SUM, SUBTRACT, or DIVIDE.");
+      throw new Error('Invalid type. Type must be SUM, SUBTRACT, or DIVIDE.');
   }
 }
 
